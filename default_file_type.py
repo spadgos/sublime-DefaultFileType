bed667976b0d204185912888376fdd7019a1ae83
Diego Trevino <diegotf30@gmail.com>
2018-01-14 11:35:07 -0600

    Refactored script to EventListener

--- default_file_type.py
+++ default_file_type.py
@@ -1,17 +1,15 @@
 import sublime
 import sublime_plugin
 
+class Listener(sublime_plugin.EventListener):
 
-class DefaultFileTypeCommand(sublime_plugin.WindowCommand):
-
-    def run(self):
-        w = self.window
+    def on_new( self, view ):
         settings = sublime.load_settings("default_file_type.sublime-settings")
-        if settings.get('use_current_syntax') and w.active_view():
-            syntax = w.active_view().settings().get('syntax')
-        else:
-            syntax = 'Packages/Default File Type 3/Syntaxes/' + settings.get('default_new_syntax')
 
-        view = w.new_file()
+        if settings.get('use_current_syntax') and view:
+            syntax = view.settings().get('syntax')
+        else:
+            syntax = settings.get('default_new_syntax')
+        print(syntax)
         # sublime.error_message("%s" % syntax)
         view.set_syntax_file(syntax)
