49c5fadf70e2f19f4ec5ffc956ef3d917f6e7a9b
Diego Trevino <diegotf30@gmail.com>
2018-01-13 23:07:02 -0600

    Added syntaxes directory
    Also, for some reason, 'use_current_file_syntax' always returned True for me,
    so I renamed it to 'use_current_syntax'

--- default_file_type.py
+++ default_file_type.py
@@ -1,5 +1,5 @@
-import sublime_plugin
 import sublime
+import sublime_plugin
 
 
 class DefaultFileTypeCommand(sublime_plugin.WindowCommand):
@@ -7,11 +7,10 @@ class DefaultFileTypeCommand(sublime_plugin.WindowCommand):
     def run(self):
         w = self.window
         settings = sublime.load_settings("default_file_type.sublime-settings")
-
-        if settings.get('use_current_file_syntax') and w.active_view():
+        if settings.get('use_current_syntax') and w.active_view():
             syntax = w.active_view().settings().get('syntax')
         else:
-            syntax = settings.get('default_new_file_syntax')
+            syntax = 'Packages/DefaultFileType/Syntaxes/' + settings.get('default_new_syntax')
 
         view = w.new_file()
         # sublime.error_message("%s" % syntax)
